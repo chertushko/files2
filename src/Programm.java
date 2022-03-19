fef37be848792d3c808f73682de405f7c18931e4
chertushko <chertushko@gmail.com>
2022-03-19 23:42:58 +0300

    Edded zipFiles

--- src/Programm.java
+++ src/Programm.java
@@ -1,12 +1,17 @@
 import java.io.*;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Programm {
 
     public static void main(String[] args) {
         StringBuilder log = new StringBuilder();
+        File dirSavegames = new File("C://Users//User//IdeaProjects//Games//savegames");
+
         createdDir(new File("C://Users//User//IdeaProjects//Games//src"), log);
         createdDir(new File("C://Users//User//IdeaProjects//Games//res"), log);
-        createdDir(new File("C://Users//User//IdeaProjects//Games//savegames"), log);
+        createdDir(dirSavegames, log);
         createdDir(new File("C://Users//User//IdeaProjects//Games//temp"), log);
         createdDir(new File("C://Users//User//IdeaProjects//Games//src//main"), log);
         createdDir(new File("C://Users//User//IdeaProjects//Games//src//test"), log);
@@ -27,11 +32,24 @@ public class Programm {
         GameProgress progress1 = new GameProgress(10, 2, 5, 55.25);
         GameProgress progress2 = new GameProgress(4, 15, 8, 107.68);
         GameProgress progress3 = new GameProgress(1, 8, 13, 498.66);
+        String path1 = dirSavegames + "//save1.dat";
+        String path2 = dirSavegames + "//save2.dat";
+        String path3 = dirSavegames + "//save3.dat";
 
-//        String path = "C://Users//User//IdeaProjects//Games//savegames";
-        saveGame("C://Users//User//IdeaProjects//Games//savegames//save1.dat", progress1);
-        saveGame("C://Users//User//IdeaProjects//Games//savegames//save2.dat", progress2);
-        saveGame("C://Users//User//IdeaProjects//Games//savegames//save3.dat", progress3);
+        String zip = dirSavegames + "//zip.zip";
+
+        saveGame(path1, progress1);
+        saveGame(path2, progress2);
+        saveGame(path3, progress3);
+
+        File[] fileList = dirSavegames.listFiles();
+
+        for (File file : fileList) {
+            if (zipFiles(zip, fileList)) {
+                file.delete();
+                System.out.println(file.getName() + " архивирован");
+            }
+        }
     }
 
     static StringBuilder createdDir(File newFile, StringBuilder sb) {
@@ -61,7 +79,23 @@ public class Programm {
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-        System.out.println(progress);
+        return true;
+    }
+
+    static boolean zipFiles(String zip, File[] files) {
+        for (File file : files) {
+            try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zip));
+                 FileInputStream fis = new FileInputStream(file)) {
+                ZipEntry entry = new ZipEntry(file.getName());
+                zos.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zos.write(buffer);
+                zos.closeEntry();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
         return true;
     }
 }
